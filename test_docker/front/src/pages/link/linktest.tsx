981b371f669cf6fd1216f3b04ce08952aebc513d
AIN1995 <abe19950311@gmail.com>
2022-09-12 17:56:52 +0900

    0912

--- test_docker/front/src/pages/link/linktest.tsx
+++ test_docker/front/src/pages/link/linktest.tsx
@@ -1,16 +1,61 @@
 import Link from "next/link"
+import {useState,useEffect} from "react";
 
 const Testlink = ()=>{
+    const [te,setTe] = useState("");
+    const [kekka,setKekka] = useState("");
+    const [aite,setAite] = useState("");
+
+    useEffect(()=>{
+        const janke = ["グー","チョキ","パー"];
+        const aitenote = Math.floor(Math.random()*janke.length);
+        setAite(janke[aitenote]);
+    },[])
+
+    console.log(aite);
+
+    const doClick = (event:any)=>{
+        setTe(event.target.value);
+    }
+
+    const doSubmit = (event:any)=>{
+        event.preventDefault();
+        if(te==="グー"&&aite==="グー") {
+            setKekka("あいこ")
+        }else  if(te==="グー"&&aite==="チョキ") {
+            setKekka("かち")
+        }else  if(te==="グー"&&aite==="パー") {
+            setKekka("まけ")
+        }else  if(te==="チョキ"&&aite==="グー") {
+            setKekka("まけ")
+        }else  if(te==="チョキ"&&aite==="チョキ") {
+            setKekka("あいこ")
+        }else  if(te==="チョキ"&&aite==="パー") {
+            setKekka("かち")
+        }else  if(te==="パー"&&aite==="グー") {
+            setKekka("かち")
+        }else  if(te==="パー"&&aite==="チョキ") {
+            setKekka("まけ")
+        }else  if(te==="パー"&&aite==="パー") {
+            setKekka("あいこ")
+        }
+    }
+
+    console.log(te);
+
     return (
         <div>   
             <Link href="../components/App">
             <a>戻るよ！！</a>
             </Link>
-            <h1>ログイン</h1>
-            <form>
-                <label>ID:</label><input type="text" /><br></br>
-                <label>パスワード:</label><input type="text" />
+            <p>ジャンケン</p>
+            <form onSubmit={doSubmit}>
+                <input type="radio" onClick={doClick} name="janken" value="グー"/>グー
+                <input type="radio" onClick={doClick} name="janken" value="チョキ"/>チョキ
+                <input type="radio" onClick={doClick} name="janken" value="パー"/>パー
+                <input type="submit" />
             </form>
+            <h1>結果{kekka}</h1>
         </div>
     )
 }
