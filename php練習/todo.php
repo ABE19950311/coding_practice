6cdca4a43483ef1313d1ca2075629a10b2d46362
ABE19950311 <abe19950311@gmail.com>
2023-01-26 20:17:48 +0900

    1/26_php_todoリストcrud動作確認済み

--- php練習/todo.php
+++ php練習/todo.php
@@ -5,8 +5,11 @@
 $pdo=new PDO("mysql:host=localhost;dbname=backtest;charset=utf8",
                 "staff","password");
 $id=$_SESSION["member"]["id"];
-$data=$pdo->query("select * from todo");
+$getdata=$pdo->query("select * from todo");
+$data=$getdata->fetchAll(PDO::FETCH_ASSOC);
 $sql=$pdo->prepare("insert into todo values(null,?,?)");
+$del=$pdo->prepare("delete from todo where id=?");
+$upd=$pdo->prepare("update todo set detail=? where id=?");
 if(empty($_POST["detail"])) {
     echo "todoを入力";
 }else if($sql->execute([$id,htmlspecialchars($_POST["detail"])])) {
@@ -15,6 +18,22 @@ if(empty($_POST["detail"])) {
     echo "失敗しました";
     ini_set('display_errors', "On");
 }
+
+if(isset($_GET["id"])) {
+    $del->execute([$_GET["id"]]);
+    echo "$_GET[id]";
+    echo "削除しました";
+}
+
+if(isset($_POST["updid"])&&isset($_POST["update"])) {
+    $upd->execute([$_POST["update"],$_POST["updid"]]);
+    echo "$_POST[update]";
+    echo "$_POST[updid]";
+    echo "更新しました";
+}
+
+echo "$_POST[update]";
+echo "$_POST[updid]";
 
 ?>
 
@@ -25,10 +44,26 @@ if(empty($_POST["detail"])) {
         <input type="submit" value="送信">
     </form>
     <?php
-    while($row=$data->fetch(PDO::FETCH_ASSOC)) { #FETCH~ フェッチモード（PDOでデータベースから取り出したデータの配列の形式を指定する）
-        print_r($row); #print_r　配列をわかりやすく表示してくれる
-        echo("<br/>");
+    echo "<table>\n";
+    echo "<tr>\n";
+    echo "<th>TODO</th>\n";
+    echo "</tr>\n";
+    foreach($data as $user) {
+                echo "<tr>\n";
+                echo "<td>" . $user["detail"] . "</td>\n";
+                echo "<td>\n";
+                echo "<a href=todo.php?id=" . $user["id"] . ">削除</a>\n";
+                echo "</td>\n";
+                echo "<td>\n";
+                echo "<form action=./todo.php method=post>";
+                echo "<input type=hidden name=updid value=$user[id]>\n";
+                echo "<input type=text name=update>\n";
+                echo "<input type=submit value=更新>\n";
+                echo "</form>";
+                echo "</td>\n";
+                echo "</tr>\n";
     }
+    echo "</table>\n";
     ?>
 </body>
 
