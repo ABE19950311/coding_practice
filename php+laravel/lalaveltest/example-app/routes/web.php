cc7f38bc07e01e927166c1a2e025e894c124c74c
ABE19950311 <abe19950311@gmail.com>
2023-02-22 21:48:45 +0900

    2/22laravelテスト続き

--- php+laravel/lalaveltest/example-app/routes/web.php
+++ php+laravel/lalaveltest/example-app/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,42 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/home", function() {
-    return view("home");
+// Sample
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
+
+
+// Tweet
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+Route::middleware('auth')->group(function () {
+    Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+        ->name('tweet.create');
+    Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
+    Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
+    Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
 });
 
-Route::get("/testres", [App\Http\Controllers\TestresController::class,"testreq"])
-    ->middleware(("guest"));
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::post("/testres", [App\Http\Controllers\TestresController::class,"testres"])
-    ->middleware(("guest"));
-
-Route::get("/register", [App\Http\Controllers\RegisterController::class,"create"])
-    ->middleware("guest")
-    ->name("register");
-
-Route::post("/register", [App\Http\Controllers\RegisterController::class,"store"])
-    ->middleware("guest");
-
-Route::get("/login", [App\Http\Controllers\LoginController::class,"index"])
-    ->middleware("guest")
-    ->name("login");
-
-Route::post("/login", [App\Http\Controllers\LoginController::class,"authenticate"])
-    ->middleware("guest");
-
-Route::get("/logout", [App\Http\Controllers\LoginController::class,"logout"])
-    ->middleware("auth")
-    ->name("logout");
-
-Route::get("/no_args",function() {
-    Artisan::call("no-args-command");
-});
-
-Route::get("/with_args",function() {
-    Artisan::call("with-args-command", [
-        "name"=>"Johann",
-        "--switch"=>true,
-    ]);
-});
-
+require __DIR__.'/auth.php';
